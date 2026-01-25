6ac9dff9b892038f22354568fa2f45c93b5618a1
Muhammad Nurhafizi <nurhafizi@innotechlabs.com.my>
2026-01-26 01:50:33 +0800

    display movies

--- index.php
+++ index.php
@@ -11,4 +11,18 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo "Connected successfully";
+$sql = "SELECT * FROM movie;";
+$result = mysqli_query($conn, $sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "- Title: " . $row["title"] . "</br>";
+        echo "- Genre: "  . $row["title"] . "</br>";
+        echo "- Duration: "  . $row["duration"] . "</br>";
+        echo "- Rating: "  . $row["rating"] . "</br>";
+        echo "- Release Date: "  . $row["release_date"] . "</br>";
+        echo "</br>";
+    }
+} else {
+    echo "0 results";
+}
