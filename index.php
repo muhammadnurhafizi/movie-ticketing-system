73d461a61023152da191a0bb4edae8664f4f8a76
Muhammad Nurhafizi <nurhafizi@innotechlabs.com.my>
2026-01-26 08:31:34 +0800

    use global variable

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 
 $conn = get_db_connection();
 
-get_movies($conn);
+get_movies();
 
-// add_movie($conn);
-// update_movie($conn);
-// get_movie($conn);
-delete_movie($conn);
+add_movie();
 
-get_movies($conn);
+get_movie();
+
+update_movie();
+
+get_movie();
+
+delete_movie();
+
+get_movies();
 
 
 
@@ -28,7 +33,9 @@ function get_db_connection() {
     return $conn;
 }
 
-function get_movies($conn) {
+function get_movies() {
+    global $conn;
+
     echo "<h3>Movie List</h3>";
 
     $stmt = $conn->prepare("SELECT * FROM movie;");
@@ -48,6 +55,108 @@ function get_movies($conn) {
         echo "0 results";
     }
 }
+
+function add_movie() {
+    global $conn;
+
+    echo "<h3>Add New Movie</h3>";
+
+    $data = [
+        "title"         => "boku no kokoro no yabai yatsu movie",
+        "genre"         => array("Comedy", "Romance"),
+        "duration"      => 105,
+        "rating"        => null,
+        "release_date"  => "2026-02-13"
+    ];
+
+    $title          = parse_title($data['title']);
+    $genre          = parse_genre($data['genre']);
+    $duration       = parse_duration($data['duration']);
+    $rating         = parse_rating($data['rating']);
+    $release_date   = parse_release_date($data['release_date']);
+
+    $stmt = $conn->prepare("INSERT INTO movie (title, genre, duration, rating, release_date) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssids", $title, $genre, $duration, $rating, $release_date);
+    if ($stmt->execute()) {
+        echo "New movie added successfully. Movie ID: " . $stmt->insert_id . "</br>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+}
+
+function get_movie() {
+    global $conn;
+
+    echo "<h3>Get Movie By ID</h3>";
+
+    $movie_id = 4;
+
+    $stmt = $conn->prepare("SELECT * FROM movie WHERE id = ?;");
+    $stmt->bind_param("i", $movie_id);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "- Title: " . format_title($row['title']) . "</br>";
+            echo "- Genre: "  . format_genre($row['genre']) . "</br>";
+            echo "- Duration: "  . format_duration($row['duration']) . "</br>";
+            echo "- Rating: "  . format_rating($row['rating']) . "</br>";
+            echo "- Release Date: "  . format_release_date($row['release_date']) . "</br>";
+            echo "</br>";
+        }
+    } else {
+        echo "0 results";
+    }
+}
+
+function update_movie() {
+    global $conn;
+
+    echo "<h3>Update Movie</h3>";
+
+    $data = [
+        "id"            => 4,
+        "title"         => "boku no kokoro no yabai yatsu movie 2",
+        "genre"         => array("Romance", "Drama"),
+        "duration"      => 120,
+        "rating"        => 8.5,
+        "release_date"  => "2027-01-23"
+    ];
+
+    $id             = (int) $data['id'];
+    $title          = parse_title($data['title']);
+    $genre          = parse_genre($data['genre']);
+    $duration       = parse_duration($data['duration']);
+    $rating         = parse_rating($data['rating']);
+    $release_date   = parse_release_date($data['release_date']);
+
+    $stmt = $conn->prepare("UPDATE movie SET title = ?, genre = ?, duration = ?, rating = ?, release_date = ? WHERE id = ?");
+    $stmt->bind_param("ssidsi", $title, $genre, $duration, $rating, $release_date, $id);
+    if ($stmt->execute()) {
+        echo "Movie updated successfully. Movie ID: " . $id . "</br>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+}
+
+function delete_movie() {
+    global $conn;
+
+    echo "<h3>Delete Movie</h3>";
+
+    $movie_id = 4;
+
+    $stmt = $conn->prepare("DELETE FROM movie WHERE id = ?;");
+    $stmt->bind_param("i", $movie_id);
+    if ($stmt->execute()) {
+        echo "Movie deleted successfully. Movie ID: " . $movie_id . "</br>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+}
+
+
 
 function format_title($title) {
     return ucwords(strtolower($title));
@@ -76,32 +185,6 @@ function format_genre($genre) {
     return ucwords(strtolower($str_genre));
 }
 
-function add_movie($conn) {
-    echo "<h3>Add New Movie</h3>";
-
-    $data = [
-        "title"         => "boku no kokoro no yabai yatsu movie",
-        "genre"         => array("Comedy", "Romance"),
-        "duration"      => 105,
-        "rating"        => null,
-        "release_date"  => "2026-02-13"
-    ];
-
-    $title          = parse_title($data['title']);
-    $genre          = parse_genre($data['genre']);
-    $duration       = parse_duration($data['duration']);
-    $rating         = parse_rating($data['rating']);
-    $release_date   = parse_release_date($data['release_date']);
-
-    $stmt = $conn->prepare("INSERT INTO movie (title, genre, duration, rating, release_date) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssids", $title, $genre, $duration, $rating, $release_date);
-    if ($stmt->execute()) {
-        echo "New movie added successfully. Movie ID: " . $stmt->insert_id . "</br>";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
-}
-
 function parse_title($title) {
     return strtolower(trim($title));
 }
@@ -128,70 +211,4 @@ function parse_release_date($release_date) {
     $release_date = trim($release_date);
     $date = DateTime::createFromFormat('Y-m-d', $release_date);
     return $date ? $date->format('Y-m-d') : null;
-}
-
-function update_movie($conn) {
-    echo "<h3>Update Movie</h3>";
-
-    $data = [
-        "id"            => 4,
-        "title"         => "boku no kokoro no yabai yatsu movie 2",
-        "genre"         => array("Romance", "Drama"),
-        "duration"      => 120,
-        "rating"        => 8.5,
-        "release_date"  => "2027-01-23"
-    ];
-
-    $id             = (int) $data['id'];
-    $title          = parse_title($data['title']);
-    $genre          = parse_genre($data['genre']);
-    $duration       = parse_duration($data['duration']);
-    $rating         = parse_rating($data['rating']);
-    $release_date   = parse_release_date($data['release_date']);
-
-    $stmt = $conn->prepare("UPDATE movie SET title = ?, genre = ?, duration = ?, rating = ?, release_date = ? WHERE id = ?");
-    $stmt->bind_param("ssidsi", $title, $genre, $duration, $rating, $release_date, $id);
-    if ($stmt->execute()) {
-        echo "Movie updated successfully. Movie ID: " . $id . "</br>";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
-}
-
-function get_movie($conn) {
-    echo "<h3>Get Movie By ID</h3>";
-
-    $movie_id = 4;
-
-    $stmt = $conn->prepare("SELECT * FROM movie WHERE id = ?;");
-    $stmt->bind_param("i", $movie_id);
-    $stmt->execute();
-
-    $result = $stmt->get_result();
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "- Title: " . format_title($row['title']) . "</br>";
-            echo "- Genre: "  . format_genre($row['genre']) . "</br>";
-            echo "- Duration: "  . format_duration($row['duration']) . "</br>";
-            echo "- Rating: "  . format_rating($row['rating']) . "</br>";
-            echo "- Release Date: "  . format_release_date($row['release_date']) . "</br>";
-            echo "</br>";
-        }
-    } else {
-        echo "0 results";
-    }
-}
-
-function delete_movie($conn) {
-    echo "<h3>Delete Movie</h3>";
-
-    $movie_id = 4;
-
-    $stmt = $conn->prepare("DELETE FROM movie WHERE id = ?;");
-    $stmt->bind_param("i", $movie_id);
-    if ($stmt->execute()) {
-        echo "Movie deleted successfully. Movie ID: " . $movie_id . "</br>";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
 }
